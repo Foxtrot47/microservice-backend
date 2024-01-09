619696eb0beb90a509a3865b99fa2e4538a0416d
Joyal Jose <jjneutron@outlook.com>
2024-01-09 20:58:48 +0530

    product-service: setup middlewares

--- apps/product-service/src/main.ts
+++ apps/product-service/src/main.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import compression from 'compression';
+import cors from 'cors';
 
 import { connectDB } from './db/mongoose';
 
@@ -8,6 +10,16 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 const app = express();
 
 connectDB();
+
+// Enable compression
+app.use(compression());
+
+// Enable CORS
+app.use(cors());
+
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
